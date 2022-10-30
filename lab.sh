43fa86ba45f8d331013918827d966aa496d4df38
omahfooz <113111602+omahfooz@users.noreply.github.com>
2022-10-30 12:39:23 +0300

    labs.sh
    s

---
+++ lab.sh
@@ -0,0 +1,205 @@
+mkdir ~/DO280-labs-v2
+cd ~/DO280-labs-v2
+mkdir 1
+cd 1
+lab install-troubleshoot start
+cp /usr/local/etc/ocp4.config .
+lab install-troubleshoot finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 2
+cd 2
+lab install-storage start
+cp /usr/local/etc/ocp4.config .
+lab install-storage finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 3
+cd 3
+lab auth-provider start
+cp /usr/local/etc/ocp4.config .
+lab auth-provider finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 4
+cd 4
+lab auth-rbac start
+cp /usr/local/etc/ocp4.config .
+lab auth-rbac finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 5
+cd 5
+lab auth-review start
+cp /usr/local/etc/ocp4.config .
+lab auth-review grade
+lab auth-review finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 6
+cd 6
+lab authorization-secrets start
+cp /usr/local/etc/ocp4.config .
+lab authorization-secrets finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 7
+cd 7
+lab authorization-scc start
+cp /usr/local/etc/ocp4.config .
+lab authorization-scc finish
+cd ..
+
+cd ~/DO280-labs-v2
+mkdir 8
+cd 8
+lab authorization-review start
+cp /usr/local/etc/ocp4.config .
+lab authorization-review finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 9
+cd 9
+lab network-sdn start
+cp /usr/local/etc/ocp4.config .
+lab network-sdn finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 10
+cd 10
+lab network-ingress start
+cp /usr/local/etc/ocp4.config .
+lab network-ingress finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 11
+cd 11
+lab network-policy start
+cp /usr/local/etc/ocp4.config .
+lab network-policy finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 12
+cd 12
+lab network-review start
+cp /usr/local/etc/ocp4.config .
+lab network-review finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 13
+cd 13
+lab schedule-pods start
+cp /usr/local/etc/ocp4.config .
+lab schedule-pods finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 14
+cd 14
+lab schedule-limit start
+cp /usr/local/etc/ocp4.config .
+lab schedule-limit finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 15
+cd 15
+lab schedule-scale start
+cp /usr/local/etc/ocp4.config .
+lab schedule-scale finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 16
+cd 16
+lab schedule-review start
+cp /usr/local/etc/ocp4.config .
+lab schedule-review finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 17
+cd 17
+lab console-admin start
+cp /usr/local/etc/ocp4.config .
+lab console-admin finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 18
+cd 18
+lab console-workloads start
+cp /usr/local/etc/ocp4.config .
+lab console-workloads finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 19
+cd 19
+lab console-metrics start
+cp /usr/local/etc/ocp4.config .
+lab console-metrics finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 20
+cd 20
+lab console-review start
+cp /usr/local/etc/ocp4.config .
+lab console-review finish
+cd ..
+
+
+
+cd ~/DO280-labs-v2
+mkdir 21
+cd 21
+lab review-troubleshoot start
+cp /usr/local/etc/ocp4.config .
+lab review-troubleshoot finish
+cd ..
+
+
+cd ~/DO280-labs-v2
+mkdir 22
+cd 22
+lab review-template start
+cp /usr/local/etc/ocp4.config .
+lab review-template finish
+cd ..
+
+
+
+
+
+
+
